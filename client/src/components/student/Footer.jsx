510a9d77a1bbc5330160c166e9450694b287f752
Julia Laba <yulia.laba13@gmail.com>
2025-06-06 00:15:07 +0300

    Changed Footer

--- client/src/components/student/Footer.jsx
+++ client/src/components/student/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
         <div className="flex flex-col md:items-start items-center w-full">
           <img src={assets.logo_dark} alt="logo" />
           <p className="mt-6 text-center md:text-left text-sm text-white/80">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.
+            We focus on giving you the best courses from top educators. Our main goal is your success!
           </p>
         </div>
 
