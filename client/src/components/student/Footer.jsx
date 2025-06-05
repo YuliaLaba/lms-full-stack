a3fe5c0385a92365317adb343c0dfdb3e5e03715
Julia Laba <yulia.laba13@gmail.com>
2025-06-06 01:06:18 +0300

    Changed Footer3

--- client/src/components/student/Footer.jsx
+++ client/src/components/student/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
     <div className="flex flex-col md:items-start items-center w-full">
       <div className="flex items-center gap-2">
         <img src={assets.logo} alt="logo" />
-        <p className="text-xl font-semibold text-white">easyStudy</p>
+        <p className="text-2xl font-semibold text-white">easyStudy</p>
       </div>
       
       <p className="mt-6 text-center md:text-left text-sm text-white/80">
